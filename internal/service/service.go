04ab07508719d59313735062a65f391f5cc80494
weberd-tech <weberd.tech@yandex.com>
2025-08-05 20:59:51 +0500

    commit 6 sprint

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func isMorseCode(text string) bool {
+	return !strings.ContainsFunc(text, func(r rune) bool {
+		return r != '.' && r != '-' && r != '/' && r != ' '
+	})
+}
+
+func ConvertMorseCode(text string) string {
+	var resultString string
+	var resultMorse string
+
+	if isMorseCode(text) {
+		resultString = morse.ToText(text)
+		return resultString
+	}
+	resultMorse = morse.ToMorse(text)
+	return resultMorse
+}
